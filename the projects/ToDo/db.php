3c3beb4f661d33cfc4725faf6d47a43f7013437a
yashravish <153241061+yashravish@users.noreply.github.com>
2024-04-05 00:48:19 -0400

    Add files via upload

---
+++ the projects/ToDo/db.php
@@ -0,0 +1,15 @@
+<?php
+// db.php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "todoDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
